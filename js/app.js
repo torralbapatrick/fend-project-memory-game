63da35eb2fc80a211317dea55ff4c704b976598f
Patrick <torralbapatrick15@gmail.com>
2018-10-28 16:36:50 -0500

    Show card when clicked

--- js/app.js
+++ js/app.js
@@ -36,3 +36,12 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+
+let allCards = document.querySelectorAll('.card');
+
+for(let card of allCards) {
+	card.addEventListener('click', function(evt) {
+		card.classList.add('open', 'show');
+	});
+}
